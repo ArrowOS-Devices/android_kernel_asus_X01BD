11e146d613b10ba1785a0c65ecd09bf573c88f1d
Jack Wu <wjack@google.com>
2019-01-11 14:16:40 +0800

    power: qpnp-fg-gen3: fix soc not keeping full after charging overnight
    The issue happened when the temperature was cool after charge_done.
    (chip->health was not POWER_SUPPLY_HEALTH_GOOD). In this condition,
    chip->charge_full was not set, and then battery level dropped.
    
    Set chip->charge_full to true if chip->health is
    POWER_SUPPLY_HEALTH_GOOD or POWER_SUPPLY_HEALTH_COOL
    or POWER_SUPPLY_HEALTH_WARM to fix it.
    
    Bug: 120606963
    Change-Id: Ia762e08826487e626dff775f074205771b908658
    Signed-off-by: Jack Wu <wjack@google.com>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -1871,7 +1871,9 @@ static int fg_charge_full_update(struct fg_chip *chip)
 		chip->charge_full);
 	printk("enter fg_charge_full_update msoc: %d bsoc: %x\n",msoc, bsoc);
 	if (chip->charge_done && !chip->charge_full) {
-		if (msoc >= 99 && chip->health == POWER_SUPPLY_HEALTH_GOOD) {
+		if (msoc >= 99 && (chip->health == POWER_SUPPLY_HEALTH_GOOD
+				|| chip->health == POWER_SUPPLY_HEALTH_COOL
+				|| chip->health == POWER_SUPPLY_HEALTH_WARM)) {
 			fg_dbg(chip, FG_STATUS, "Setting charge_full to true\n");
 			chip->charge_full = true;
 			/*
